a03edf903e540a3d340892e63c18b8fd802846ec
Jahirul <jahirul76@gmail.com>
2015-04-14 11:35:09 +0100

    updated codebook, readme and comments in code

--- run_analysis.R
+++ run_analysis.R
@@ -1,13 +1,15 @@
+## The purpose of script is to create tidy dataset from 'UCI HAR Dataset' downloaded 
+## from the link below.
+##   https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+##
+## Extract the ZIP file to ./data inside th getwd() directory.
+##
+## This script expects data files to be availabel under './data/UCI HAR Dataset' directory.
 
-# Merges the training and the test sets to create one data set.
-# Extracts only the measurements on the mean and standard deviation for each measurement. 
-# Uses descriptive activity names to name the activities in the data set
-# Appropriately labels the data set with descriptive variable names. 
-# 
-# From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
 
-# Location of all files used for this excercise
+# Location of all files used for tiday dataset
+# =======================================
 
 # training dataset
 trainDataFile <- "./data/UCI HAR Dataset/train/X_train.txt"
@@ -20,12 +22,10 @@ testLabelFile <- "./data/UCI HAR Dataset/test/y_test.txt"
 testSubjectFile <- "./data/UCI HAR Dataset/test/subject_test.txt"
 
 # global files 
-fFeatures <- "./data/UCI HAR Dataset/features.txt"
-fActivity <- "./data/UCI HAR Dataset/activity_labels.txt"
+featuresFile <- "./data/UCI HAR Dataset/features.txt"
+activityFile <- "./data/UCI HAR Dataset/activity_labels.txt"
+# =======================================
 
-# load features and activity description 
-features <- read.table(fFeatures)
-activity <- read.table(fActivity)
 
 # load training dataset and descriptions
 trainData <- read.table(trainDataFile, colClasses=rep("numeric", 561))
@@ -37,26 +37,28 @@ testData <- read.table(testDataFile, colClasses=rep("numeric", 561))
 testLabel <- read.table(testLabelFile)
 testSubjData <- read.table(testSubjectFile)
 
-# apply columm header/names
+
+# load features and activity description 
+features <- read.table(featuresFile)
+activity <- read.table(activityFile)
+
+
+# name all variables
 names(trainData) <- as.character(features$V2)
 names(testData) <- as.character(features$V2)
 
-#add lable column to training dataset
-trainData <- cbind(SubjectId=trainSubjData$V1, Label=trainLabel$V1, trainData)
-#names(trainData) <- c("SubjectId", "Label", as.character(features$V2))
+#join subject and activity columns to training and test dataset
+trainData <- cbind(Subject=trainSubjData$V1, Activity=trainLabel$V1, trainData)
+testData <- cbind(Subject=testSubjData$V1, Activity=testLabel$V1, testData)
 
-#add lable column to test dataset
-testData <- cbind(SubjectId=testSubjData$V1, Label=testLabel$V1, testData)
-##names(testData) <- c("SubjectId", "Label", as.character(features$V2))
-
-# join training and test dataset together
+# merge both datasets together
 merged_data <- rbind(trainData, testData)
 
-#subset and remove all column except mean(), std(), SubjetcId and Label columns
-merged_data <- merged_data[,grep("(mean|std)\\(\\)|SubjectId|Label", names(merged_data))]
+# subset and remove all column except mean(), std(), subject and activity columns
+merged_data <- merged_data[,grep("(mean|std)\\(\\)|Subject|Activity", names(merged_data))]
 
-# Replace Label with descriptive Level names: WALKING, SITTING 
-merged_data$Label <- activity[merged_data$Label, c(2)]
+# Replace activity id with descriptive activity name such as WALKING, SITTING 
+merged_data$Activity <- activity[merged_data$Activity, c(2)]
 
 # tidy up variable names
 names(merged_data) <- gsub("\\(\\)", "", names(merged_data))
@@ -64,23 +66,24 @@ names(merged_data) <- gsub("mean", "Mean", names(merged_data))
 names(merged_data) <- gsub("std", "Std", names(merged_data))
 
 # write 1st dataset to disk
-write.table(merged_data, "merged_data.txt", row.name=FALSE) 
-
+#write.table(merged_data, "merged_data.txt", row.name=FALSE) 
 
 
 # Create a second, independent tidy data set with the average 
-## of each variable for each activity and each subject.
-
+# of each variable for each activity and each subject.
+# ======================================================
 library(plyr)
 
-# Column means for all but the subject and activity columns
-limitedColMeans <- function(data, exclude=c(1,2)) { colMeans(data[,-exclude]) }
+merged_means <- ddply(merged_data, 
+                      .(Subject, Activity),  
+                      function(dat, exclude=c(1,2)) colMeans(dat[,-exclude])
+                      )
 
-tidyMeans <- ddply(merged_data, .(SubjectId, Label), limitedColMeans)
-
-names(tidyMeans)[-c(1,2)] <- paste0("Mean", names(tidyMeans)[-c(1,2)])
+names(merged_means)[-c(1,2)] <- paste0("Mean", names(merged_means)[-c(1,2)])
+names(merged_means)[-c(1,2)] <- gsub("Meant", "MeanT", names(merged_means)[-c(1,2)])
+names(merged_means)[-c(1,2)] <- gsub("Meanf", "MeanF", names(merged_means)[-c(1,2)])
 
 # Write file
-write.table(tidyMeans, "tidyMeans.txt", row.names = FALSE)
+write.table(merged_means, "merged_means.txt", row.names = FALSE)
 
-
+# ======================================================
